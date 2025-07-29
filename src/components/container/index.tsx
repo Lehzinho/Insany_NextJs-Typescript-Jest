65f38641a2597109fefc80a885dfa034556947bd
Alexandre Toulios <99737712+Lehzinho@users.noreply.github.com>
2025-07-29 07:27:41 -0300

    feat: creates a wrapping `container` for the app

---
+++ src/components/container/index.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from "react";
+import * as S from "./styles";
+
+interface ContainerProps {
+  children: ReactNode;
+}
+
+const Container = ({ children }: ContainerProps) => {
+  return <S.Container>{children}</S.Container>;
+};
+
+export default Container;
