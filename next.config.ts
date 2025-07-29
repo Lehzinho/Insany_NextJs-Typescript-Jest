7eea541d2b45400ffaf54d242cab800b1bc9fccf
Alexandre Toulios <99737712+Lehzinho@users.noreply.github.com>
2025-07-29 11:37:47 -0300

    ci: configs `hidration` and `image`

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  onError: (err: any, errorInfo: any) => {
+    if (err.message?.includes("data-np-intersection-state")) {
+      return;
+    }
+  },
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
   },
   images: {
-    domains: ["comprarcuecas.com.br"],
+    domains: ["images.unsplash.com"],
   },
 };
 
