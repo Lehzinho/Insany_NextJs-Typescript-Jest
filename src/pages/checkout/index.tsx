37fc7038833eb8baea999a922c0122bb617f6082
Alexandre Toulios <99737712+Lehzinho@users.noreply.github.com>
2025-07-30 09:08:17 -0300

    feat: adds `checkout` pages and styles

---
+++ src/pages/checkout/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import * as S from "@/styles/pages/checkout.styles";
+const Checkout = () => {
+  return <S.CheckoutContainer>checkout</S.CheckoutContainer>;
+};
+
+export default Checkout;
---
+++ src/styles/pages/checkout.styles.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const CheckoutContainer = styled.main``;
+
+export const teste = styled.div``;
